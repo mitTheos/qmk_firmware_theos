28ebd6bf6ee89619fb6ff073f6fc0692a0eb0586
mitTheos <tim2kaspar@gmail.com>
2025-02-19 12:54:24 +0100

    update keymap

--- keyboards/sofle/keymaps/theosofle/config.h
+++ keyboards/sofle/keymaps/theosofle/config.h
@@ -6,7 +6,8 @@
 #define TRI_LAYER_LOWER_LAYER 2
 #define TRI_LAYER_UPPER_LAYER 3
 #define TRI_LAYER_ADJUST_LAYER 4
-
+/*
 #define ENCODERS_PAD_A { D3, B6 }
 #define ENCODERS_PAD_B { D2, B0 }
 #define ENCODER_RESOLUTION 4
+*/
--- keyboards/sofle/keymaps/theosofle/rules.mk
+++ keyboards/sofle/keymaps/theosofle/rules.mk
@@ -1,7 +1,7 @@
 TRI_LAYER_ENABLE = yes
-ENCODER_ENABLE = yes
+# ENCODER_ENABLE = yes
 # ENCODER_MAP_ENABLE = yes
-WPM_ENABLE = yes  # Enable word per minute counter
-LTO_ENABLE = yes  # Makes hex file smaller
-OLED_ENABLE = yes # Enable OLEDs
-RGBLIGHT_ENABLE = no # to reduce firmware size
+# WPM_ENABLE = yes  # Enable word per minute counter
+# LTO_ENABLE = yes  # Makes hex file smaller
+# OLED_ENABLE = yes # Enable OLEDs
+# RGBLIGHT_ENABLE = no # to reduce firmware size
