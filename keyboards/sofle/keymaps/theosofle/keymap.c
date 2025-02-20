bb3cd7a87e8a146e0320f14114c37d0d38bb2802
mitTheos <tim2kaspar@gmail.com>
2025-02-20 18:22:41 +0100

    update keymap

--- keyboards/sofle/keymaps/theosofle/keymap.c
+++ keyboards/sofle/keymaps/theosofle/keymap.c
@@ -14,29 +14,19 @@
   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
   */
 
-#define ANIM_INVERT false
-#define ANIM_RENDER_WPM true
-#define FAST_TYPE_WPM 45 //Switch to fast animation when over words per minute
-
- /*
-#ifdef OLED_ENABLE
-#include "demon.c"
-#endif
-*/
-
 #include QMK_KEYBOARD_H
 #if __has_include("keymap.h")
 #    include "keymap.h"
 #endif
 
- // from qmk configurator
+// from qmk configurator
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(KC_LSFT, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PSCR, KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_GRV, KC_BSPC, KC_A, KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, KC_O, KC_QUOT, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_WSCH, KC_MPLY, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, KC_LGUI, KC_DOWN, KC_UP, KC_SPC, LT(2,KC_ESC), LT(1,KC_ENT), KC_RSFT, KC_LEFT, KC_RGHT, KC_LALT),
     [1] = LAYOUT(KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_NO, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_NO, KC_COLN, KC_F12, KC_DEL, KC_LBRC, KC_LCBR, KC_LPRN, KC_MINS, KC_NO, KC_NO, KC_EQL, KC_RPRN, KC_RCBR, KC_RBRC, KC_DQUO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LT, KC_GT, KC_QUES, KC_PIPE, KC_NO, KC_PGDN, KC_PGUP, KC_LGUI, KC_COLN, KC_NO, KC_NO, KC_HOME, KC_END, KC_NO),
     [2] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F13, KC_F14, KC_F15, KC_F16, KC_NO, KC_NO, LGUI(KC_F13), LGUI(KC_F14), LGUI(KC_F15), LGUI(KC_F16), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F17, KC_F18, KC_MPRV, KC_MNXT, KC_MPLY)
 };
 
- #if defined(ENCODER_MAP_ENABLE)
+#if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(KC_UP, KC_DOWN),  ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)  },
     [1] = { ENCODER_CCW_CW(KC_J, KC_K),  ENCODER_CCW_CW(KC_H, KC_L)  },
@@ -60,21 +50,3 @@ bool oled_task_user(void) {
     return false;
 }
 #endif
-
-/*
-#ifdef OLED_ENABLE
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master()) {
-    return OLED_ROTATION_180;
-  }
-  return rotation;
-}
-
-bool oled_task_user(void) {
-  if (!is_keyboard_master()) {
-    oled_render_anim();
-  }
-  return false;
-}
-#endif
-*/
